cddea40386f838ffeaf6f708f02c421f258913dd
godu <arthur.weber@viacesi.fr>
2016-03-21 15:56:03 +0100

    Adds squirrel bin

--- bin/squirrel.js
+++ bin/squirrel.js
@@ -1,39 +1,71 @@
 #! /usr/bin/env node
 var path = require('path');
-var minimist = require('minimist')
-var _ = require('lodash/fp');
+var minimist = require('minimist');
+var fs = require('fs');
+var Promise = require('bluebird');
 var createDriver = require('../src/etcd-driver');
 
 var argz = minimist(process.argv.slice(2));
 
+var source = path.join(process.cwd(), argz._[0]);
+var destination = path.join('/', argz._[1]);
+
 var driver = createDriver({
     hosts: argz.hosts ? argz.hosts.split(',') : undefined,
-    cwd: argz.cwd
+    cwd: destination
 });
 
-var files = require(path.join(process.cwd(), argz.file));
-
-driver.list().then(function(node) {
-    return _.pipe(
-        _.map(function(node) {
-            if (node.dir)
-                return driver.rmdir(node.key);
-            return driver.del(node.key);
+function syncEntry(cwd) {
+    return Promise.all([
+        Promise.fromCallback(function(cb) {
+            fs.stat(path.join(source, cwd), cb);
+        }),
+        driver.get(cwd).catch(function(err) {
+            return Promise.resolve(null);
         })
-    )(node.nodes || []);
-}).then(function() {
-    return Promise.all(
-        _.pipe(
-            _.toPairs,
-            _.map(_.spread(function(key, value) {
-                return driver.set(key, value);
-            }))
-        )(files)
-    );
-}).then(function() {
-    return driver.list()
-}).then(function(node) {
-    process.stdout.write(JSON.stringify(node, null, 4));
+    ]).then(function(results) {
+        var stat = results[0];
+        var node = results[1];
+
+        if (stat.isDirectory()) {
+            (function() {
+                if (!node) return driver.mkdir(cwd);
+                if (node.dir) return Promise.resolve();
+                return driver.del(cwd).then(function() {
+                    return driver.mkdir(cwd);
+                });
+            })().then(function() {
+                return Promise.fromCallback(function(cb) {
+                    fs.readdir(path.join(source, cwd), cb);
+                });
+            }).then(function(files) {
+                return Promise.all(files.map(function(file) {
+                    syncEntry(path.join(cwd, file));
+                }));
+            });
+        }
+        else {
+            return (node && node.dir ? driver.rmdir(cwd) : Promise.resolve()).then(function() {
+                return Promise.fromCallback(function(cb) {
+                    return fs.readFile(path.join(source, cwd), cb);
+                });
+            }).then(function(data) {
+                try {
+                    return JSON.parse(data);
+                } catch (err) {
+                    return data;
+                }
+            }).then(function(data) {
+                return driver.set(cwd, data);
+            });
+        }
+    });
+}
+
+console.log('Copy ' + source + ' local directory to ' + destination + ' etcd directory');
+
+syncEntry('').then(function() {
+    console.log('Sync');
 }, function(err) {
-    process.stderr.write(err.stack);
+    console.error('Error:', err.stack);
 });
