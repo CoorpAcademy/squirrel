2fefe010df423f83e1bc2a90b0fd39aaa3edc102
godu <arthur.weber@viacesi.fr>
2016-05-02 15:38:46 +0200

    Adds improvements according @jfmengels review

--- src/api.js
+++ src/api.js
@@ -3,7 +3,8 @@ import {
   find,
   getOr,
   keys,
-  startsWith
+  startsWith,
+  curry
 } from 'lodash/fp';
 import createDebug from 'debug';
 
@@ -12,9 +13,7 @@ const debug = createDebug('squirrel');
 const createAPI = store => {
   const getBy = (index, key) => {
     debug(`getBy: ${index} => ${key}`);
-    return store('indexes').then(indexes =>
-      getOr(null, [index, key, 'value'], indexes)
-    );
+    return store('indexes').then(getOr(null, [index, key, 'value']));
   };
 
   const getAll = index => {
@@ -29,14 +28,14 @@ const createAPI = store => {
     return store('node').then(_get(path));
   };
 
-  const _get = _path => node => {
+  const _get = curry((_path, node) => {
     if (!node) return null;
     if (path.relative(node.key, _path) === '') return node;
 
-    return _get(_path)(find(function(child) {
+    return _get(_path, find(function(child) {
       return startsWith(child.key, _path);
     }, node.nodes));
-  };
+  });
 
   return {
     getBy,
