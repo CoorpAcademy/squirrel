b72ee56b61a76c1ca8efcfc86d86244d4aa6b7ae
Arthur Weber <arthur.weber@viacesi.fr>
2016-05-24 09:21:06 +0200

    Replace eslint-config-coorp with eslint-plugin-coorp (#33)

--- src/bin/etcd.js
+++ src/bin/etcd.js
@@ -1,18 +1,12 @@
 import {readFileSync} from 'fs';
 import {resolve} from 'path';
-import {assign, pick, pipe, set} from 'lodash/fp';
+import {pick} from 'lodash/fp';
 import Etcd from 'node-etcd';
 
 export default argz => {
   const hosts = argz.hosts || 'http://localhost:2379';
 
-  const etcdOptions = pipe(
-    assign(
-      argz.ca ?
-        set('ca', readFileSync(resolve(process.cwd(), argz.ca)), {}) :
-        {}
-    )
-  )({});
+  const etcdOptions = !argz.ca ? {} : {ca: readFileSync(resolve(process.cwd(), argz.ca))};
 
   const client = new Etcd(
     hosts.split(','),
