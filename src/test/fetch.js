61e68ff034dcfd3816bf09e33727ca285cf738cc
travis-ci <travis@travis-ci.org>
2016-07-04 14:51:26 +0200

    Removes etcd helper

--- src/test/fetch.js
+++ src/test/fetch.js
@@ -1,15 +1,17 @@
 import test from 'ava';
 import {pipe, fill, map, concat} from 'lodash/fp';
 
-import createEtcd from './helpers/etcd';
 import createFetch$ from '../fetch';
 
 import emptyRoot from './fixtures/empty-root';
 
 test('should fetch nodes', t => {
-  const client = createEtcd({
-    get: [[null, emptyRoot]]
-  });
+  const getMocks = [[null, emptyRoot]];
+
+  const client = {
+    get: (cwd, options, cb) => cb(...getMocks.shift())
+  };
+
   const fetch$ = createFetch$(client, '/');
 
   const expected = [emptyRoot];
@@ -25,13 +27,16 @@ test('should fetch nodes', t => {
 });
 
 test('should retry on error', t => {
-  const client = createEtcd({
-    get: pipe(
-      fill(new Error),
-      map(err => [err]),
-      concat([[null, emptyRoot]])
-    )(Array(10))
-  });
+  const getMocks = pipe(
+    fill(new Error),
+    map(err => [err]),
+    concat([[null, emptyRoot]])
+  )(Array(10));
+
+  const client = {
+    get: (cwd, options, cb) => cb(...getMocks.shift())
+  };
+
   const fetch$ = createFetch$(client, '/');
 
   return new Promise((resolve, reject) => {
