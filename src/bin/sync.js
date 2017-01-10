ac874abbdeffb5f51ba980c61cd0a878a7adc03b
godu <arthur.weber@viacesi.fr>
2017-01-10 12:58:59 +0100

    Fixes lint

--- src/bin/sync.js
+++ src/bin/sync.js
@@ -13,6 +13,6 @@ const pathETCD = resolve('/', argz._[1]);
 const client = makeEtcdClient(argz);
 
 sync$(client, pathFS, pathETCD).toPromise().then( // eslint-disable-line promise/catch-or-return
-  () => process.stdout.write(`The end.\n`),
+  () => process.stdout.write('The end.\n'),
   err => process.stderr.write(`${err.stack}\n`)
 );
