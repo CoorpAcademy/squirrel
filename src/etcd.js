298f07505ffc4eb20d565135250d2bd814e9861a
godu <arthur.weber@viacesi.fr>
2017-10-30 16:55:19 +0100

    Create folder if not exists

--- src/etcd.js
+++ src/etcd.js
@@ -1,12 +1,20 @@
+import {Observable} from 'rxjs';
 import createWatcher$ from './watch';
 import createResync$ from './resync';
 
 const createEtcd$ = (client, cwd) => {
+  const mkdir$ = Observable.bindNodeCallback(client.mkdir)(cwd)
+    .catch(err => {
+      if (err.errorCode === 102) return Observable.empty();
+      throw err;
+    })
+    .ignoreElements();
+
   const events$ = createWatcher$(client, cwd).startWith({
     action: 'resync'
   });
 
-  return createResync$(client, cwd, events$);
+  return Observable.merge(mkdir$, createResync$(client, cwd, events$));
 };
 
 export default createEtcd$;
--- src/test/etcd.js
+++ src/test/etcd.js
@@ -1,4 +1,5 @@
 import {EventEmitter} from 'events'; // eslint-disable-line fp/no-events
+import {before} from 'lodash/fp';
 import test from 'ava';
 import createEtcd$ from '../etcd';
 import createEtcdMock from '../util/test/helpers/etcd';
@@ -31,18 +32,38 @@ test('should composite events observable', async t => {
       }
     ]
   ];
+  const mkdirMocks = [[null, true]];
 
   const watcher = new EventEmitter();
+
+  const emitEvents = before(
+    () =>
+      [setEvent, deleteEvent, resyncEvent, deleteEvent, setEvent].forEach(event =>
+        watcher.emit(event.action, event)
+      ),
+    3
+  );
+  watcher.addListener = function(event, listener) {
+    const detach = EventEmitter.prototype.addListener.call(this, event, listener);
+    setTimeout(emitEvents, 0);
+    return detach;
+  };
   watcher.stop = () => {};
 
   const client = createEtcdMock(
     {
-      get: getMocks
+      get: getMocks,
+      mkdir: mkdirMocks
     },
     () => watcher
   );
 
   const events$ = createEtcd$(client, '/');
+
+  const eventsP = events$
+    .take(6)
+    .toArray()
+    .toPromise();
 
   const expected = [
     {
@@ -67,15 +88,67 @@ test('should composite events observable', async t => {
     setEvent
   ];
 
+  const events = await eventsP;
+  t.deepEqual(events, expected);
+});
+
+test('should ignore if cwd already exists', async t => {
+  const getMocks = [
+    [
+      null,
+      {
+        action: 'get',
+        node: {
+          key: '/',
+          dir: true,
+          nodes: [setEvent.node]
+        }
+      }
+    ]
+  ];
+  const NotAFile = new Error('Not a file');
+  NotAFile.errorCode = 102;
+
+  const mkdirMocks = [[NotAFile, null]];
+
+  const client = createEtcdMock({
+    get: getMocks,
+    mkdir: mkdirMocks
+  });
+
+  const events$ = createEtcd$(client, '/');
+
   const eventsP = events$
-    .take(6)
+    .take(1)
     .toArray()
     .toPromise();
 
-  [setEvent, deleteEvent, resyncEvent, deleteEvent, setEvent].forEach(event =>
-    watcher.emit(event.action, event)
-  );
+  const expected = [
+    {
+      action: 'get',
+      node: {
+        key: '/',
+        dir: true,
+        nodes: [setEvent.node]
+      }
+    }
+  ];
 
   const events = await eventsP;
   t.deepEqual(events, expected);
+});
+
+test('should throw error if error occured', t => {
+  const UnknownError = new Error();
+  const mkdirMocks = [[UnknownError, null]];
+
+  const client = createEtcdMock({
+    mkdir: mkdirMocks
+  });
+
+  const events$ = createEtcd$(client, '/');
+
+  const eventsP = events$.toPromise();
+
+  return t.throws(eventsP);
 });
