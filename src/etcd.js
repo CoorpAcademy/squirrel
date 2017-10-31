bb1c4d0bd967ced53e44314916a6aceb4d7fa711
godu <arthur.weber@viacesi.fr>
2017-10-31 16:19:49 +0100

    Fixes mkdir

--- src/etcd.js
+++ src/etcd.js
@@ -3,7 +3,7 @@ import createWatcher$ from './watch';
 import createResync$ from './resync';
 
 const createEtcd$ = (client, cwd) => {
-  const mkdir$ = Observable.bindNodeCallback(client.mkdir)(cwd)
+  const mkdir$ = Observable.bindNodeCallback((...args) => client.mkdir(...args))(cwd)
     .catch(err => {
       if (err.errorCode === 102) return Observable.empty();
       throw err;
